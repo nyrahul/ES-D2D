7683318af938ebe177c4637b768b8893aa795928
Rahul Jadhav <nyrahul@gmail.com>
2019-03-05 18:18:24 +0530

    :qx

--- cmds.sh
+++ cmds.sh
@@ -1,2 +1,4 @@
-wget -O /dev/null http://10.42.0.1:8000/1GB.file 
+wget -O /dev/null http://10.42.0.1:8000/1GB.file
 sudo ifconfig wlo1 mtu 2304
+python -m SimpleHTTPServer
+dd if=/dev/zero of=1GB.file bs=MB count=1000
